042ef8d8bff99d48cf62d25aa413694b282450ef
souvikjs01 <souvik741156@gmail.com>
2026-01-15 09:38:31 +0530

    restaurant & menu

---
+++ zomato-design/cpp/models/MenuItem.h
@@ -0,0 +1,45 @@
+#ifndef MENUITEM_H
+#define MENUITEM_H
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class MenuItem {
+private:
+    string code;
+    string name;
+    int price;
+public:
+    MenuItem(string code, string name, int price) {
+        this->code = code;
+        this->name = name;
+        this->price = price;
+    }
+    string getCode() const {
+        return code;
+    }
+
+    void setCode(const string &c) {
+        code = c;
+    } 
+
+    string getName() const {
+        return name;
+    }
+
+    void setName(const string &n) {
+        name = n;
+    } 
+
+    int getPrice() const {
+        return price;
+    }
+
+    void setPrice(const int &p) {
+        price = p;
+    } 
+
+};
+
+#endif
