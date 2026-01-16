4a8e02a2906ead124892499dfbbf7184a6ce6c82
souvikjs01 <souvik741156@gmail.com>
2026-01-16 12:25:03 +0530

    cart class

---
+++ zomato-design/cpp/models/Cart.h
@@ -0,0 +1,50 @@
+#ifndef CART_H
+#define CART_H
+#include<iostream>
+#include<string>
+#include<vector>
+#include "Restaurant.h"
+#include "./MenuItem.h"
+
+using namespace std;
+
+class Cart {
+private:
+    Restaurant* res;
+    vector<MenuItem> items;
+public:
+    Cart() {
+        res = nullptr;
+    }
+
+    void addItem(const MenuItem &it) {
+        if(!res) {
+            cout << "Select a restaurant first then add item" << endl;
+            return;
+        }
+        items.push_back(it);
+    }
+    double getTotalCost() const {
+        double sum = 0;
+        for (const auto& it : items) {
+            sum += it.getPrice();
+        }
+        return sum;
+    }
+    bool isEmpty() {
+        return (!res || items.empty()); 
+    }
+
+    // getter setter
+    void setRestaurant(Restaurant* r) {
+        res = r;
+    }
+    Restaurant* getRestaurant() const {
+        return res;
+    }
+    const vector<MenuItem>& getItems() const {
+        return items;
+    }
+};
+
+#endif
