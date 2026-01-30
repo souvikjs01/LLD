46b9b4609ba1bf6d5121f24c900faed65123e287
souvikjs01 <souvik741156@gmail.com>
2026-01-30 10:50:19 +0530

    time utils

---
+++ zomato-design/cpp/utils/TimeUtils.h
@@ -0,0 +1,21 @@
+#ifndef TIME_UTILS_H
+#define TIME_UTILS_H
+
+#include<string>
+#include<ctime>
+using namespace std;
+
+class TimeUtils {
+public:
+    static string getCurrentTime() {
+        time_t now = time(0);
+        char* dt = ctime(&now);
+        string s(dt);
+        if(!s.empty() && s.back() == '\n') {
+            s.pop_back();
+        }
+        return s;
+    }
+};
+
+#endif
